2fc5e39426fdbc9fde7819275da7b5a0eb27ee0c
Jelte Homminga <jeltehomminga@gmail.com>
2019-03-08 10:48:22 +0100

    make it better again

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-require('dotenv').config()
+require('dotenv').config();
 const createError = require('http-errors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
